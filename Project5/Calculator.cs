5107e85439bf69661d1f82bf74303826d65c11de
SelfJ19 <SELFJ1@ETSU.EDU>
2023-04-27 14:33:09 -0400

    Add project files.

---
+++ Project5/Calculator.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project5
+{
+    internal class Calculator
+    {
+        double answer = 0;
+        // calcState = dispatch[cmd](p1,p2)
+        // stack of answers to pop the previous result
+        public double GetCurrentValue()
+        {
+            return answer;
+        }
+
+        #region Addition()
+        public void Addition(string p1, string p2)
+        {
+            double a = double.Parse(p1);
+            double b = double.Parse(p2);
+            answer = a + b;
+        }
+        #endregion
+
+        #region Subtraction()
+        public void Subtraction(string p1, string p2)
+        {
+            double a = double.Parse(p1);
+            double b = double.Parse(p2);
+            answer = a - b;
+        }
+        #endregion
+
+        #region Multiply()
+        public void Multiplication(string p1, string p2)
+        {
+            double a = double.Parse(p1);
+            double b = double.Parse(p2);
+            answer = a * b;
+        }
+        #endregion
+
+        #region Divide()
+        public void Division(string p1, string p2)
+        {
+            double a = double.Parse(p1);
+            double b = double.Parse(p2);
+            answer = a / b;
+        }
+        #endregion
+
+        #region Mod()
+        public void Modulus(string p1, string p2)
+        {
+            double a = double.Parse(p1);
+            double b = double.Parse(p2);
+            answer = a % b;
+        }
+        #endregion
+
+        #region Square()
+        public void Square(string p1)
+        {
+            double a = double.Parse(p1);
+            answer = Math.Pow(a, 2);
+        }
+        #endregion
+
+        #region Sqrt()
+        public void SquareRoot(string p1)
+        {
+            double a = double.Parse(p1);
+            answer = Math.Sqrt(a);
+        }
+        #endregion
+
+        #region Exponential()
+        public void Exponentiate(string p1, string p2)
+        {
+            double a = double.Parse(p1);
+            double b = double.Parse(p2);
+            answer = Math.Pow(a, b);
+        }
+        #endregion
+
+        #region Factorial()
+        public void Factorial(string p1)
+        {
+            double total = 1;
+            double a = double.Parse(p1);
+            if (a < 0)
+            {
+                throw new Exception("Can not do the factorial of a negative value.");
+            }
+            
+                for (double i = a; i > 0; i--)
+                {
+                   total *= i;
+                }
+            answer = total;
+            
+        }
+        #endregion
+    }
+}
---
+++ Project5/Program.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.CSharp.RuntimeBinder;
+using Project5;
+using System.Security.Cryptography.X509Certificates;
+
+public class Program
+{
+    public static Dictionary<string, dynamic> dispatchTable = new();
+
+    public static void Main(string[] args)
+    {
+        bool isRunning = true;
+        Calculator calculator = new Calculator();
+        dispatchTable["add"] = new Action<string, string>((p1, p2) => calculator.Addition(p1, p2));
+        dispatchTable["subtract"] = new Action<string, string>((p1, p2) => calculator.Subtraction(p1, p2));
+        dispatchTable["multiply"] = new Action<string, string>((p1, p2) => calculator.Multiplication(p1, p2));
+        dispatchTable["divide"] = new Action<string, string>((p1, p2) => calculator.Division(p1, p2));
+        dispatchTable["mod"] = new Action<string, string>((p1, p2) => calculator.Modulus(p1, p2));
+        dispatchTable["square"] = new Action<string>((p1) => calculator.Square(p1));
+        dispatchTable["squareRoot"] = new Action<string>((p1) => calculator.SquareRoot(p1));
+        dispatchTable["exponentiate"] = new Action<string, string>((p1, p2) => calculator.Exponentiate(p1, p2));
+        dispatchTable["factorial"] = new Action<string>((p1) => calculator.Factorial(p1));
+        dispatchTable["exit"] = new Action(() => { isRunning = false; });
+        
+        while(isRunning)
+        {
+            try
+            {
+                Console.WriteLine("Add [value1] [value2]");
+                Console.WriteLine("Subtract [value1] [value2]");
+                Console.WriteLine("Multiply [value1] [value2]");
+                Console.WriteLine("Divide [value1] [value2]");
+                Console.WriteLine("Mod [value1] [value2]");
+                Console.WriteLine("Square [value]");
+                Console.WriteLine("SquareRoot [value]");
+                Console.WriteLine("Exponentiate [value1] [value2]");
+                Console.WriteLine("Factorial [value]");
+                Console.WriteLine("Exit");
+
+                string[] input = Console.ReadLine().ToString().Split(" ");
+                if (input.Length == 3)
+                {
+                    dispatchTable[input[0]](input[1], input[2]);
+                    Console.WriteLine(calculator.GetCurrentValue());
+                }
+                else if (input.Length == 2)
+                {
+                    dispatchTable[input[0]](input[1]);
+                    Console.WriteLine(calculator.GetCurrentValue());
+                }
+                else if (input.Length == 1)
+                {
+                    dispatchTable[input[0]]();
+                }
+            } catch (RuntimeBinderException e) { Console.WriteLine("You need to enter a value or values based on which option you choose."); }
+        }
+    }
+}
