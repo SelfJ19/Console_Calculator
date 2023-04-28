c41e20f60df4f4095880b93b124a8d4c24617299
SelfJ19 <SELFJ1@ETSU.EDU>
2023-04-28 18:34:14 -0400

    added methods that do calculations on the previous answer

--- Project5/Calculator.cs
+++ Project5/Calculator.cs
@@ -10,81 +10,147 @@ namespace Project5
 {
     internal class Calculator
     {
-        double answer = 0;
-        // calcState = dispatch[cmd](p1,p2)
+        Dictionary<string, double> variables = new();
         // stack of answers to pop the previous result
+        #region GetCurrentValue()
         public double GetCurrentValue()
         {
-            return answer;
+            return variables.ContainsKey("currentValue") ? variables["currentValue"] : 0;
         }
+        #endregion
 
-        #region Addition()
+        #region Store()
+        public void Store(string key, string value)
+        {
+            double x;
+            double.TryParse(value, out x);
+            variables[key] = x;
+        }
+        #endregion
+
+        #region Parse()
+        public double Parse(string value)
+        {
+            double x;
+            if (variables.ContainsKey(value))
+            {
+                x = variables[value];
+            }
+            else
+            {
+                double.TryParse(value, out x);
+            }
+            return x;
+        }
+        #endregion
+
+        #region Addition(p1)
+        public void Addition(string p1)
+        {
+            double b = Parse(p1);
+            variables["currentValue"] = this.GetCurrentValue() + b;
+        }
+        #endregion
+
+        #region Add(p1,p2)
         public void Addition(string p1, string p2)
         {
-            double a = double.Parse(p1);
-            double b = double.Parse(p2);
-            answer = a + b;
+            double a = Parse(p1);
+            double b = Parse(p2);
+            variables["currentValue"] = a + b;
         }
         #endregion
 
-        #region Subtraction()
+        #region Subtraction(p1)
+        public void Subtraction(string p1)
+        {
+            double b = Parse(p1);
+            variables["currentValue"] = this.GetCurrentValue() - b;
+        }
+        #endregion
+
+        #region Subtraction(p1,p2)
         public void Subtraction(string p1, string p2)
         {
-            double a = double.Parse(p1);
-            double b = double.Parse(p2);
-            answer = a - b;
+            double a = Parse(p1);
+            double b = Parse(p2);
+            variables["currentValue"] = a - b;
         }
         #endregion
 
-        #region Multiply()
+        #region Multiplication(p1)
+        public void Multiplication(string p1)
+        {
+            double b = Parse(p1);
+            variables["currentValue"] = this.GetCurrentValue() * b;
+        }
+        #endregion
+
+        #region Multiply(p1,p2)
         public void Multiplication(string p1, string p2)
         {
-            double a = double.Parse(p1);
-            double b = double.Parse(p2);
-            answer = a * b;
+            double a = Parse(p1);
+            double b = Parse(p2);
+            variables["currentValue"] = a * b;
         }
         #endregion
 
-        #region Divide()
+        #region Divide(p1)
+        public void Division(string p1)
+        {
+            double b = Parse(p1);
+            variables["currentValue"] = this.GetCurrentValue() / b;
+        }
+        #endregion
+
+        #region Divide(p1,p2)
         public void Division(string p1, string p2)
         {
-            double a = double.Parse(p1);
-            double b = double.Parse(p2);
-            answer = a / b;
+            double a = Parse(p1);
+            double b = Parse(p2);
+            variables["currentValue"] = a / b;
         }
         #endregion
 
-        #region Mod()
+        #region Mod(p1)
+        public void Modulus(string p1)
+        {
+            double b = Parse(p1);
+            variables["currentValue"] = this.GetCurrentValue() % b;
+        }
+        #endregion
+
+        #region Mod(p1,p2)
         public void Modulus(string p1, string p2)
         {
-            double a = double.Parse(p1);
-            double b = double.Parse(p2);
-            answer = a % b;
+            double a = Parse(p1);
+            double b = Parse(p2);
+            variables["currentValue"] = a % b;
         }
         #endregion
 
         #region Square()
         public void Square(string p1)
         {
-            double a = double.Parse(p1);
-            answer = Math.Pow(a, 2);
+            double a = Parse(p1);
+            variables["currentValue"] = Math.Pow(a, 2);
         }
         #endregion
 
         #region Sqrt()
         public void SquareRoot(string p1)
         {
-            double a = double.Parse(p1);
-            answer = Math.Sqrt(a);
+            double a = Parse(p1);
+            variables["currentValue"] = Math.Sqrt(a);
         }
         #endregion
 
         #region Exponential()
         public void Exponentiate(string p1, string p2)
         {
-            double a = double.Parse(p1);
-            double b = double.Parse(p2);
-            answer = Math.Pow(a, b);
+            double a = Parse(p1);
+            double b = Parse(p2);
+            variables["currentValue"] = Math.Pow(a, b);
         }
         #endregion
 
@@ -92,7 +158,7 @@ namespace Project5
         public void Factorial(string p1)
         {
             double total = 1;
-            double a = double.Parse(p1);
+            double a = Parse(p1);
             if (a < 0)
             {
                 throw new Exception("Can not do the factorial of a negative value.");
@@ -102,7 +168,7 @@ namespace Project5
                 {
                    total *= i;
                 }
-            answer = total;
+            variables["currentValue"] = total;
             
         }
         #endregion
--- Project5/Program.cs
+++ Project5/Program.cs
@@ -10,48 +10,58 @@ public class Program
     {
         bool isRunning = true;
         Calculator calculator = new Calculator();
+
         dispatchTable["add"] = new Action<string, string>((p1, p2) => calculator.Addition(p1, p2));
         dispatchTable["subtract"] = new Action<string, string>((p1, p2) => calculator.Subtraction(p1, p2));
         dispatchTable["multiply"] = new Action<string, string>((p1, p2) => calculator.Multiplication(p1, p2));
         dispatchTable["divide"] = new Action<string, string>((p1, p2) => calculator.Division(p1, p2));
         dispatchTable["mod"] = new Action<string, string>((p1, p2) => calculator.Modulus(p1, p2));
-        dispatchTable["square"] = new Action<string>((p1) => calculator.Square(p1));
-        dispatchTable["squareRoot"] = new Action<string>((p1) => calculator.SquareRoot(p1));
+        dispatchTable["sq"] = new Action<string>((p1) => calculator.Square(p1));
+        dispatchTable["sqrt"] = new Action<string>((p1) => calculator.SquareRoot(p1));
         dispatchTable["exponentiate"] = new Action<string, string>((p1, p2) => calculator.Exponentiate(p1, p2));
         dispatchTable["factorial"] = new Action<string>((p1) => calculator.Factorial(p1));
+        dispatchTable["store"] = new Action<string, string>((p1, p2) => calculator.Store(p1, p2));
         dispatchTable["exit"] = new Action(() => { isRunning = false; });
         
         while(isRunning)
         {
             try
             {
+                Console.WriteLine("Please select from the following options.");
+                Console.WriteLine("Enter your command as the function first & then the value(s) you want to perform the operation on.");
+                Console.WriteLine("PLease be sure to include the spaces.\n");
+                Console.WriteLine("===================== Calculator Options =========================\n");
                 Console.WriteLine("Add [value1] [value2]");
                 Console.WriteLine("Subtract [value1] [value2]");
                 Console.WriteLine("Multiply [value1] [value2]");
                 Console.WriteLine("Divide [value1] [value2]");
                 Console.WriteLine("Mod [value1] [value2]");
-                Console.WriteLine("Square [value]");
-                Console.WriteLine("SquareRoot [value]");
+                Console.WriteLine("Sq [value]");
+                Console.WriteLine("Sqrt [value]");
                 Console.WriteLine("Exponentiate [value1] [value2]");
                 Console.WriteLine("Factorial [value]");
+                Console.WriteLine("Store [key] [value]");
                 Console.WriteLine("Exit");
+                Console.WriteLine("==================================================================\n");
 
-                string[] input = Console.ReadLine().ToString().Split(" ");
+                string[] input = Console.ReadLine().ToLower().Split(" ");
                 if (input.Length == 3)
                 {
                     dispatchTable[input[0]](input[1], input[2]);
-                    Console.WriteLine(calculator.GetCurrentValue());
+                    Console.WriteLine(calculator.GetCurrentValue()+"\n");
                 }
                 else if (input.Length == 2)
                 {
                     dispatchTable[input[0]](input[1]);
-                    Console.WriteLine(calculator.GetCurrentValue());
+                    Console.WriteLine(calculator.GetCurrentValue()+"\n");
                 }
                 else if (input.Length == 1)
                 {
                     dispatchTable[input[0]]();
                 }
-            } catch (RuntimeBinderException e) { Console.WriteLine("You need to enter a value or values based on which option you choose."); }
+            } catch (RuntimeBinderException e) { Console.WriteLine("You need to enter a value or values based on which option you choose.\n"); }
+            catch (KeyNotFoundException e) { Console.WriteLine("That is not a valid operation.\n"); }
+            catch (Exception e){ Console.WriteLine(e.Message); }
         }
     }
 }
